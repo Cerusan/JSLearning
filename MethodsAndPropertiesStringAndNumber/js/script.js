7277e29461eaaf19dd25c6a6ff6ee69e9791234d
Cerus-laptop <vladtastenov@gmail.com>
2025-07-01 10:23:32 +0500

    lesson about Methods and Properties for Strings and Numbers

---
+++ MethodsAndPropertiesStringAndNumber/js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const str = `test`;
+//const arr = [1, 2, 4];
+
+//console.log(arr.length);
+//console.log(str.length);
+
+//console.log(str[2]);
+
+//console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+console.log(str);
+
+const fruit = `Some fruit`;
+
+console.log(fruit.indexOf(`fruit`));
+
+const world = "Hello world!";
+
+console.log(world.slice(0, 5));
+console.log(world.substring(6, 11));
+console.log(world.substr(3, 6));
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+const test = `12.2px`;
+
+console.log(parseInt(test));
+console.log(parseFloat(test));
